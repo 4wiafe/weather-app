e4090c0bf330848c8b20aafb0e0c79aa12ba0263
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-09-21 10:35:49 +0000

    Add async function to get data from fetch response

--- src/dataMapper.js
+++ src/dataMapper.js
@@ -1,1 +1,14 @@
 import { getWeather } from "./apiService.js"; 
+
+async function mapData(city) {
+  const weatherData = await getWeather(city);
+  const collected = {
+    city: weatherData.address,
+    date: new Date(weatherData.currentConditions.datetimeEpoch * 1000).toDateString(),
+    icon: weatherData.currentConditions.icon,
+    temp: weatherData.currentConditions.temp,
+    description: weatherData.currentConditions.conditions,
+    humidity: weatherData.currentConditions.humidity,
+    windspeed: weatherData.currentConditions.windspeed
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import "./styles.css"; 
 import bgImage from "./assets/images/weather_bg.jpg";
+import { mapData } from "./dataMapper.js";
+
+mapData("accra")
