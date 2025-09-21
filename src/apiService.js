79f7e18e21a2a91ec29e222941d43d9f03f47ee3
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-09-21 00:51:33 +0000

    Add async function to get weather data

--- src/apiService.js
+++ src/apiService.js
@@ -1,1 +1,11 @@
- 
+async function getWeather() {
+  try {
+    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/accra?key=9F5MJ9SWTS6TM6Q2B3U3GFYMF");
+    const weatherData = await response.json();
+    console.log(weatherData);
+  } catch (error) {
+    throw new Error("Oops! Something went wrong.", error);
+  }
+} 
+
+export { getWeather };
