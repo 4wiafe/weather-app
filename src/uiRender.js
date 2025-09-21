48ae5c1f9ed8f9b63cfd99e1a1383429b922f73b
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-09-21 22:52:56 +0000

    Display city name and date

--- src/uiRender.js
+++ src/uiRender.js
@@ -1,11 +1,30 @@
 import { mapData } from "./dataMapper.js"; 
 
-const cityInput = document.querySelector(".search-bar");
-const trimmed = cityInput.trim();
 const weatherInfo = document.querySelector(".weather-info");
 
-async function renderData() {
-  const data = await mapData(trimmed);
-
+async function renderData(inputValue) {
   weatherInfo.innerHTML = "";
+  
+  const data = await mapData(inputValue);
+
+  const cityDate = document.createElement("div");
+  cityDate.className = "city-date";
+  const cityName = document.createElement("div");
+  cityName.className = "city";
+  const cityText = document.createElement("p");
+  cityText.textContent = `📍${data.city}`;
+
+  const date = document.createElement("div");
+  date.className = "date";
+  const dateText = document.createElement("p");
+  dateText.textContent = `${data.date}`;
+
+  cityName.appendChild(cityText);
+  date.appendChild(dateText);
+  cityDate.append(cityName, date);
+  weatherInfo.append(cityDate);
+
+  console.log(weatherInfo);
 }
+
+export { renderData };
