f2a9c071ea07fe7421964624af298a24440e043d
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-09-21 15:26:42 +0000

    Add async function renderData
    Function awaits mapData to retrieve data to be displayed

--- src/uiRender.js
+++ src/uiRender.js
@@ -2,3 +2,8 @@ import { mapData } from "./dataMapper.js";
 
 const cityInput = document.querySelector(".search-bar");
 const trimmed = cityInput.trim();
+const weatherInfo = document.querySelector(".weather-info");
+
+async function renderData() {
+  const data = await mapData(trimmed);
+}
